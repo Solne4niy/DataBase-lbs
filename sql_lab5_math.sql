bbd4330a20ae148437ce68317f23aee1736fa7b1
Solne4niy <95656015+Solne4niy@users.noreply.github.com>
2021-12-07 11:38:30 +0200

    Update sql_lab5_math.sql

--- sql_lab5_math.sql
+++ sql_lab5_math.sql
@@ -1,30 +1,30 @@
-create table PERSON_TYPE_1
-    as select id, name from PERSON_TYPE
+create table EVENT_TYPE_1
+    as select id, name from EVENT_TYPE
         where id <= 4;
         
-create table PERSON_TYPE_2
-    as select id, name from PERSON_TYPE
+create table EVENT_TYPE_2
+    as select id, name from EVENT_TYPE
         where id > 2 and id < 7;
 
 
 
-select * from PERSON_TYPE_1
-    union select * from PERSON_TYPE_2;
+select * from EVENT_TYPE_1
+    union select * from EVENT_TYPE_2;
     
-select * from PERSON_TYPE_1
-    union all select * from PERSON_TYPE_2;
+select * from EVENT_TYPE_1
+    union all select * from EVENT_TYPE_2;
 
 
 
-select * from PERSON_TYPE_1
-    intersect select * from PERSON_TYPE_2;
+select * from EVENT_TYPE_1
+    intersect select * from EVENT_TYPE_2;
 	
 	
    
-select * from PERSON_TYPE_1
-    minus select * from PERSON_TYPE_2; 
+select * from EVENT_TYPE_1
+    minus select * from EVENT_TYPE_2; 
     
 	
 	
 
-select * from PERSON_TYPE_1, PERSON_TYPE_2;
+select * from EVENT_TYPE_1, EVENT_TYPE_2;
